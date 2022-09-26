cca42d77da2a01b7040f0a1fc383e43033847696
bemrekalkan <bemre.kalkan@gmail.com>
2022-09-26 22:22:33 +0300

    search field added

--- quiz/views.py
+++ quiz/views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 
 from .models import (
     Category,
@@ -12,14 +13,14 @@ from .serializers import (
     QuizSerializer
 )
 
-
 class CategoryList(generics.ListAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
-
 class QuizList(generics.ListAPIView):
     queryset = Quiz.objects.all()
     serializer_class = QuizSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
     filterset_fields = ['category']
+    search_fields = ['title']
+
