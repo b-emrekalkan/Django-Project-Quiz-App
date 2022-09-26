3529b7d7917c935313ce4162cd2e1e42812e6a08
bemrekalkan <bemre.kalkan@gmail.com>
2022-09-26 20:55:58 +0300

    nested_admin used

--- quiz/admin.py
+++ quiz/admin.py
@@ -4,10 +4,25 @@ from .models import (
     Category,
     Quiz,
     Question,
-    Option,
+    Option
 )
-#! Register your models here.
+class OptionAdmin(nested_admin.NestedTabularInline):
+    model = Option
+    extra = 5
+
+class QuestionAdmin(nested_admin.NestedTabularInline):
+    model = Question
+    inlines = [OptionAdmin]
+    extra = 5
+    max_num = 20
+
+
+class QuizAdmin(nested_admin.NestedModelAdmin):
+    model = Quiz
+    inlines = [QuestionAdmin]
+
+
 admin.site.register(Category)
-admin.site.register(Quiz)
+admin.site.register(Quiz, QuizAdmin)
 admin.site.register(Question)
 admin.site.register(Option)
